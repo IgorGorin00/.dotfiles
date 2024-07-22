d140fe80ce0b78d583445c6bda1202a556299633
Igor <gorin.igor.00@gmail.com>
2024-07-22 20:58:14 +0300

    snippets

--- nvim/lua/verius00/lazy/lsp.lua
+++ nvim/lua/verius00/lazy/lsp.lua
@@ -14,6 +14,15 @@ return {
         "j-hui/fidget.nvim",
     },
     config = function()
+        local cmp = require("cmp")
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities()
+        )
         require("fidget").setup({})
         require("mason").setup()
         require("mason-lspconfig").setup({
@@ -24,11 +33,14 @@ return {
             },
             handlers = {
                 function (server_name)
-                    require("lspconfig")[server_name].setup {}
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
                 end,
                 ["lua_ls"] = function ()
                     local lspconfig = require("lspconfig")
                     lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
                         settings = {
                             Lua = {
                                 diagnostics = {
@@ -41,8 +53,6 @@ return {
             },
         })
 
-        local cmp = require("cmp")
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
             snippet = {
